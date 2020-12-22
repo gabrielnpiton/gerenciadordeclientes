274bbf3841a30b704861c4916e15e9f1e7d420bc
Gabriel Piton <gabrielprogramador1@gmail.com>
2020-12-21 22:54:01 -0300

    Primeiro commit

---
+++ GerenciadorDeClientes.Dominio/Cliente/Entidade/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciadorDeClientes.Dominio.Cliente.Entidade
+{
+    public class Cliente
+    {
+        //Propriedades
+        public string Nome { get; set; }
+        public string Cnpj { get; set; }
+        public Endereco  Endereco { get; set; }
+
+    }
+}
---
+++ GerenciadorDeClientes.Dominio/Cliente/Entidade/Endereco.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciadorDeClientes.Dominio.Cliente.Entidade
+{
+    public class Endereco
+    {
+        public string Rua { get; set; }
+        public string Bairro { get; set; }
+        public int Numero { get; set; }
+        public int Cep { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+    }
+}
